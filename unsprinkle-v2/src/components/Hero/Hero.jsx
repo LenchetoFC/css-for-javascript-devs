4816f5c4c7e122b42819b85bd32a78107c761d4d
LenchetoFC <lenchetoramirez16@gmail.com>
2025-07-15 17:43:00 -0400

    CSS Module 6 Unsprinkle Workshop

---
+++ unsprinkle-v2/src/components/Hero/Hero.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const Hero = () => {
+  return (
+    <Wrapper>
+      <HeroImage
+        src="/images/hero-img.avif"
+        alt="An example of photos distributed through this site (Light brown cat staring at your with its piercing blue eyes)"
+      />
+      <Swoop src="/swoop.svg" alt="" />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  position: relative;
+  height: 60vh;
+  min-height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  background: hsl(0deg 0% 1%);
+`;
+
+const HeroImage = styled.img`
+  display: block;
+  width: 500px;
+  height: 500px;
+  max-height: 100%;
+`;
+
+const Swoop = styled.img`
+  position: absolute;
+  left: 0;
+  right: 0;
+  /*
+    Overhang by a couple px to prevent any pixel-rounding
+    display issues
+  */
+  bottom: -2px;
+  width: 100%;
+`;
+
+export default Hero;
